73ee007fba9d683cbd4e24833d326b027b535e5d
happygame406 <maxpro406@gmail.com>
2026-05-25 01:05:33 +0300

    fix: add run_job_task decorator

--- app/tasks/executor.py
+++ app/tasks/executor.py
@@ -63,6 +63,14 @@ async def run_job_task(self, job_id: UUID) -> None:
         )
         log.success("Set status -> DONE")
 
+        await update_job_state(
+            session=session,
+            job=job,
+            status=JobStatus.DONE,
+            finished_at=datetime.now(UTC),
+            result=result
+        )
+        log.success("Set status -> DONE")
 
-# Для совместимости
+
 execute_job = run_job_task
